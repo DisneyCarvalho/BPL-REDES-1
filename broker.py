9a5bf8e9233fa0b182eb7eeaabbb798156ca2e59
DisneyCarvalho <114960349+DisneyCarvalho@users.noreply.github.com>
2024-05-09 14:07:11 -0300

    Update broker.py

--- broker.py
+++ broker.py
@@ -115,7 +115,7 @@ class Broker:
                             self.dispositivos.get((i),{}).pop(j, None)
                             self.verifica_ips()
 
-                    except TimeoutError and socket.timeout:
+                    except TimeoutError and socket.timeout and ConnectionResetError:
                         print("Timeout cancela")
                         print(self.dispositivos)
                         self.dispositivos.get((i),{}).pop(j, None)
